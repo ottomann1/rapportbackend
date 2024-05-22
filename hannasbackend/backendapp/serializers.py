91d7e2726af8adada30477b3a17f8d8798b7c7ef
ottomann1 <otto.kostmann@hotmail.com>
2024-05-22 12:59:18 +0200

    easy serializer

--- hannasbackend/backendapp/serializers.py
+++ hannasbackend/backendapp/serializers.py
@@ -1,42 +1,38 @@
 from rest_framework import serializers
-from .models import Report, ReportTemplate, Question, Answer
+from .models import Company, UserProfile, ReportTemplate, Question, Report, Answer
 
 
-class AnswerSerializer(serializers.ModelSerializer):
-    question = serializers.ReadOnlyField(source="question.text")
-
+class CompanySerializer(serializers.ModelSerializer):
     class Meta:
-        model = Answer
-        fields = ["question", "answer", "explanation"]
+        model = Company
+        fields = "__all__"
 
 
-class QuestionSerializer(serializers.ModelSerializer):
-    answers = AnswerSerializer(many=True, read_only=True)
-
+class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Question
-        fields = ["id", "text", "answers"]
-
-
-class ReportSerializer(serializers.ModelSerializer):
-    answers = AnswerSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Report
-        fields = [
-            "id",
-            "report_title",
-            "submitted_on",
-            "last_updated",
-            "template",
-            "answers",
-        ]
+        model = UserProfile
+        fields = "__all__"
 
 
 class ReportTemplateSerializer(serializers.ModelSerializer):
-    questions = QuestionSerializer(many=True, read_only=True)
-    reports = ReportSerializer(many=True, read_only=True)
-
     class Meta:
         model = ReportTemplate
-        fields = ["id", "name", "questions", "reports"]
+        fields = "__all__"
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = "__all__"
+
+
+class ReportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Report
+        fields = "__all__"
+
+
+class AnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Answer
+        fields = "__all__"
